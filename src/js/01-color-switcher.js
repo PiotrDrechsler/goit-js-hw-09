546a16e8b72bf985223161f52691630a1ba47891
PiotrDrechsler <pdrech@gmail.com>
2022-10-16 16:44:17 +0200

    Update task-01
    task01 - ready to check

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        console.log(`Interval with id ${timerId} changed body color for ${getRandomHexColor()}!`);
+        body.style.backgroundColor = getRandomHexColor()}, 1000);
+        startBtn.disabled = true;
+        stopBtn.disabled = false;
+        startBtn.style.cursor = "default";
+        stopBtn.style.cursor = "pointer";
+  });
+  
+  
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    console.log(`Interval with id ${timerId} has stopped!`);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    startBtn.style.cursor = "pointer";
+    stopBtn.style.cursor = "default";
+  });
